789c5ad6da56c0e0c075b47891b297aeeef4378a
Dornox <69315835+krushna06@users.noreply.github.com>
2024-01-08 19:26:48 +0530

    Uploaded Files

---
+++ src/assets/languages.ts
@@ -0,0 +1,8 @@
+import en from "@/assets/locales/en.json";
+import pirate from "@/assets/locales/pirate.json";
+
+export const locales = {
+  en,
+  pirate,
+};
+export type Locales = keyof typeof locales;
---
+++ src/setup/constants.ts
@@ -0,0 +1,6 @@
+export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
+export const DISCORD_LINK = "https://discord.gg/2yrfTt29B2";
+export const GITHUB_LINK = "https://github.com/krushna06";
+export const DONATION_LINK = "https://ko-fi.com/krushna06";
+export const GA_ID = "G-DMTJ75Y19N";
+export const BACKEND_URL = "https://backend.fenxtube.io";
