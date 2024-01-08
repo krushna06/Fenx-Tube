ace863cf35f45797667e0904adb53c6cb0934d92
Dornox <69315835+krushna06@users.noreply.github.com>
2024-01-08 20:52:16 +0530

    Update constants.ts

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -3,4 +3,4 @@ export const DISCORD_LINK = "https://discord.gg/2yrfTt29B2";
 export const GITHUB_LINK = "https://github.com/krushna06";
 export const DONATION_LINK = "https://ko-fi.com/krushna06";
 export const GA_ID = "G-DMTJ75Y19N";
-export const BACKEND_URL = "https://backend.fenxtube.io";
+export const BACKEND_URL = "https://backend.movie-web.app";
